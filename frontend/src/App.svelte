f3231634ee0dd3095c68b0750813d2c3c3edea09
Mateusz Kruk <krukm634@gmail.com>
2023-05-12 23:02:17 +0200

    basic app

---
+++ frontend/src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Map from './lib/Map.svelte';
+</script>
+
+<main>
+  <h1>Stan Rynku nieruchomości w Polsce</h1>
+  <p>Stan na 05.2023</p>
+  <section>
+    <Map/>
+  </section>
+  <footer>
+    <p>2023, made by Mateusz Kruk</p>
+  </footer>
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+  }
+  footer {
+    height: fit-content;
+    margin-top: auto;
+    margin: .5rem;
+  }
+  section{
+    flex-grow: 1;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 2rem;
+  }
+</style>
