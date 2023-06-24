3f4ba541b4e9e010c256406ac1921de10252a5f8
Mateusz Kruk <krukm634@gmail.com>
2023-06-24 17:12:18 +0200

    styling changes

--- frontend/src/views/App.svelte
+++ frontend/src/views/App.svelte
@@ -90,6 +90,7 @@
                 <ConnectedScatterChart data={$chartData}/>
             {/if}
         </div>
+        <h6 style="opacity: 0.5;">Made with &lt;3 by Mateusz Kruk, Seweryn Jarco, Dawid	Goc and, Kacper	Koniuszy</h6>
     </div>
 </div>
 
@@ -141,5 +142,10 @@
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+
+  h6 {
+    margin-top: auto;
+    text-align: right;
   }
 </style>
